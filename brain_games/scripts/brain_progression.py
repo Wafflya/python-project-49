81da67d0665799cba4e481b737cff865b2c6d91b
Marat <vmarat95@gmail.com>
2023-04-12 23:17:37 +0300

    fix: delete imports from brain_progression

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -1,11 +1,5 @@
 #!/usr/bin/env python3
-
-from brain_games.cli import welcome_user
 from brain_games.games.progression import main
-from brain_games.service import ask_question, check_answer, create_progression
-
-
-
 
 if __name__ == "__main__":
     main()
